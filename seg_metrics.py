fc154b2f2aaf2212b83e5e52db93a77ba1c2100f
Leonardo Araujo <leonardoaraujo.santos@gmail.com>
2019-09-01 13:00:20 +0000

    Adding metrics, still not there...

--- seg_metrics.py
+++ seg_metrics.py
@@ -9,6 +9,24 @@ from torchvision.utils import make_grid
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim import lr_scheduler
 
+
+SMOOTH = 1e-6
+
+def iou_binary(outputs: torch.Tensor, labels: torch.Tensor):
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most probably
+    # be with the BATCH x 1 x H x W shape
+    outputs = (outputs > 0).squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    
+    intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum((1, 2))         # Will be zzero if both are 0
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    
+    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    
+    return thresholded  # Or thresholded.mean() if you are interested in average across the batch
+
 # This version uses hard threshold so it can't be used on the loss function but is precise 
 # to verify the real IoU
 # Reference: https://www.kaggle.com/iezepov/fast-iou-scoring-metric-in-pytorch-and-numpy
